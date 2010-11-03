9f196c3c9ad3a410c44791cb892b8a103ba18a39
Ilinca Vintila <ilinca.vintila@it.su.se>
2010-11-03 16:37:22 +0100

    Added empty tests for SignupController

---
+++ test/unit/se/su/it/signup/SignupControllerTests.groovy
@@ -0,0 +1,17 @@
+package se.su.it.signup
+
+import grails.test.*
+
+class SignupControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
