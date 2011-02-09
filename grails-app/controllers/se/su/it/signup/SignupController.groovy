ce7f4694d72b3fe13762807a8f4ff769a392e86a
Marcus Dubois <marcus@it.su.se>
2011-02-09 10:47:40 +0100

    remove config for now

--- grails-app/controllers/se/su/it/signup/SignupController.groovy
+++ grails-app/controllers/se/su/it/signup/SignupController.groovy
@@ -5,8 +5,12 @@ package se.su.it.signup
 class SignupController {
 
   def index = {
+    def link = 'https://public.it.secure.su.se/shibboleth/Shibboleth.sso/WAYF/studera.nu/produktion?target=https://sukattool-web1.it.su.se:1043/foo/signup/setup'
+
+    [link: link]
   }
 
   def setup = {
+    def pause = 42
   }
 }
