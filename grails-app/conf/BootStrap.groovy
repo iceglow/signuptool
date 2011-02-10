d3d253ba1139471058cc74a111702e0de45db674
Richard Nyberg <rnyberg@it.su.se>
2011-02-10 10:45:36 +0100

    Add configuration for kerberos authentication.
    Might not be correct.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,35 @@
 class BootStrap {
-  
+
+  def configService
+
   def init = { servletContext ->
+
+    configService.registerSection("properties")
+    configService.registerValueToSection("properties", "javax.security.auth.useSubjectCredsOnly", "false")
+    configService.registerValueToSection("properties", "java.security.krb5.kdc", "kerberos.su.se")
+    configService.registerValueToSection("properties", "java.security.krb5.realm", "SU.SE")
+
+    configService.registerSection("jaas")
+    configService.registerValueToSection("jaas", "client", "TRUE")
+    configService.registerValueToSection("jaas", "storeKey", "true")
+    configService.registerValueToSection("jaas", "doNotPrompt", "true")
+    configService.registerValueToSection("jaas", "useKeyTab", "true")
+    configService.registerValueToSection("jaas", "realm", "SU.SE")
+    configService.registerValueToSection("jaas", "debug", "false")
+    configService.registerValueToSection("jaas", "keyTab", "/etc/krb5.keytab_su-portal-dev")
+    configService.registerValueToSection("jaas", "principal", "su-portal-dev@SU.SE")
+
+    java.security.Security.setProperty("login.configuration.provider", "se.su.it.signuptool.Krb5Configuration")
+
+    // Get the properties section and register as system properties
+    Properties p = configService.getSectionAsProperties("properties")
+    Properties sysprop = System.getProperties()
+    for(String key : p.propertyNames()) {
+      sysprop.setProperty(key, p.getProperty(key))
+    }
+    System.setProperties(sysprop)
   }
+
   def destroy = {
   }
 } 
