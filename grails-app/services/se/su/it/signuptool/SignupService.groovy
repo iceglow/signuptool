5069ec1f5d94901d0a5defbdaefcd57dd231b1f7
Richard Nyberg <rnyberg@it.su.se>
2011-05-19 12:35:22 +0200

    Update enableUser for changes in enrollUser.

--- grails-app/services/se/su/it/signuptool/SignupService.groovy
+++ grails-app/services/se/su/it/signuptool/SignupService.groovy
@@ -11,19 +11,8 @@ class SignupService {
   static transactional = false
 
   Map enableAccount(ShibAttributes attrs) throws Exception {
-    def vo = null
-    vo = WsMethodService?.findEnrolledUserByNIN(attrs.nin)
-    if (!vo && attrs.domain =~ /student.su.se/) {
-      vo = WsMethodService?.enrollUser(attrs.domain, attrs.givenName, attrs.sn, attrs.nin)
-    }
-
-    if (!vo?.uid && !vo?.password) {
-      throw new Exception("Aktiveringen av ditt konto misslyckades.")
-    }
-
-    WsMethodService?.enableBasicServices(vo.uid)
-    def mail =  WsMethodService?.getMail(vo.uid)
-
+    def vo = WsMethodService.enrollUser(attrs.domain, attrs.givenName, attrs.sn, attrs.nin)
+    def mail =  WsMethodService.getMail(vo.uid)
     [vo: vo, mail: mail]
   }
 
