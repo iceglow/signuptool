fa8e198f776a562ea50856a8be8243dea77a8389
Ilinca Vintila <ilinca.vintila@it.su.se>
2011-08-29 12:28:49 +0200

    Better error-messages when encountering validation-errors from vhs.

--- grails-app/controllers/se/su/it/signuptool/CroakController.groovy
+++ grails-app/controllers/se/su/it/signuptool/CroakController.groovy
@@ -17,7 +17,7 @@ class CroakController {
   }
 
   def studeraNuAccountSetupError = {
-    render(view: 'errorView', model: [errorTitle: message(code: 'accountActivationError.pageHeader'), errorMessage: message(code: 'accountActivationError.info.text')])
+    render(view: 'errorView', model: [errorTitle: message(code: 'studeraNuActivationError.pageHeader'), errorMessage: message(code: 'studeraNuAccountSetupError.info.text')])
   }
   def studeraNuResetPasswordError = {
     render(view: 'errorView', model: [errorTitle: message(code: 'accountResetError.pageHeader'), errorMessage: message(code: 'accountResetError.info.text'), tryAgainAction: 'resetaccount'])
