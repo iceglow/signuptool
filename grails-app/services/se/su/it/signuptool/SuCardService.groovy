1503703f76b72903b7277ef7558f5d86d1541b3c
Michael Schenström <michael@nodewalker.com>
2013-06-13 13:01:56 +0200

    DEVIDM-19: SuCardService
    Refactoring and tests

--- grails-app/services/se/su/it/signuptool/SuCardService.groovy
+++ grails-app/services/se/su/it/signuptool/SuCardService.groovy
@@ -1,5 +1,6 @@
 package se.su.it.signuptool
 
+import se.su.it.sucard.client.ArrayOfCardOrderVO
 import se.su.it.sucard.client.CardOrderFacadePortType
 import se.su.it.sucard.client.CardOrderVO
 import se.su.it.ws.commons.WSLocatorFactory
@@ -13,41 +14,56 @@ class SuCardService {
   CardOrderFacadePortType cardOrderFacade = null
 
   public CardOrderVO[] getCardOrdersForUser(String uid) {
-    try {
-      CardOrderFacadePortType facade = getFacade(CardOrderFacadePortType.class)
-      return facade.getCardOrdersForUser(uid).getCardOrderVO()
-    } catch (Throwable exception) {
-      log.error("Problem getting cardorders: ${exception.getMessage()}",exception)
-      return null
+    CardOrderVO[] cardOrderVOs = null
+
+    if (uid && (uid.length() > 0)) {
+
+      try {
+        CardOrderFacadePortType facade = getCardOrderFacade()
+        ArrayOfCardOrderVO  arrayOfCardOrderVO = facade.getCardOrdersForUser(uid)
+        cardOrderVOs = arrayOfCardOrderVO.getCardOrderVO()
+
+      } catch (Throwable exception) {
+        log.error("Problem getting cardorders: ${exception.getMessage()}",exception)
+        return null
+      }
     }
+
+    return cardOrderVOs
   }
 
   public boolean orderCard(CardOrderVO cardOrderVO) {
-    try {
-      getCardOrderFacade().orderCard(cardOrderVO)
-      return true
-    } catch (Throwable exception) {
-      log.error("Problem ordering card: ${exception.getMessage()}",exception)
-      return false
-    }
-  }
+    boolean cardOrdered = false
 
-  private WSLocatorFactory getWsLocator() {
-    if (wsLocatorFactory == null) {
-      wsLocatorFactory = WSLocatorFactory.instance(configService.getSectionAsProperties("WS"))
+    if (cardOrderVO) {
+
+      try {
+        CardOrderFacadePortType facade = getCardOrderFacade()
+        facade.orderCard(cardOrderVO)
+        cardOrdered = true
+
+      } catch (Throwable exception) {
+        log.error("Problem ordering card: ${exception.getMessage()}",exception)
+        return false
+      }
     }
-    return wsLocatorFactory
+
+    return cardOrdered
   }
 
   private CardOrderFacadePortType getCardOrderFacade() {
-    synchronized (cardOrderFacade) {
-      if(!cardOrderFacade) {
-        WSLocatorFactory wsLocator = getWsLocator()
-        Remote serviceFacade = wsLocator.getService(clazz)
-        cardOrderFacade = (CardOrderFacadePortType) serviceFacade
-      }
+    if(!cardOrderFacade) {
+      WSLocatorFactory wsLocator = getWsLocator()
+      Remote serviceFacade = wsLocator.getService(CardOrderFacadePortType.class)
+      cardOrderFacade = (CardOrderFacadePortType) serviceFacade
     }
     return cardOrderFacade
   }
 
+  private WSLocatorFactory getWsLocator() {
+    if (!wsLocatorFactory) {
+      wsLocatorFactory = WSLocatorFactory.instance(configService.getSectionAsProperties("WS"))
+    }
+    return wsLocatorFactory
+  }
 }
