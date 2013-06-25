0647028c370d5c9add5592cde64b618d796e0201
Tommy Andersson <tommy.andersson@su.se>
2013-06-25 14:16:00 +0200

    DEVIDM-25: Added tests for ActivateAccountAndCardService

--- grails-app/services/se/su/it/signuptool/ActivateAccountAndCardService.groovy
+++ grails-app/services/se/su/it/signuptool/ActivateAccountAndCardService.groovy
@@ -4,18 +4,11 @@ class ActivateAccountAndCardService implements Serializable {
   /** Needed if we want to use this service in the flow. */
   static transactional = false
 
-  def ladokService
-  def sukatService
-
   private final emailPattern = /[_A-Za-z0-9-]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})/
 
   /** Checks if user has any active cards or active order for a card */
   public boolean canOrderCard() {
     return true
-  }
-
-  public String getForwardAddress(String pnr) {
-    ladokService.findForwardAddressSuggestionForPnr(pnr)
   }
 
   public boolean validateForwardAddress(String forwardAddress) {
--- test/unit/se/su/it/signuptool/ActivateAccountAndCardServiceSpec.groovy
+++ test/unit/se/su/it/signuptool/ActivateAccountAndCardServiceSpec.groovy
@@ -2,6 +2,7 @@ package se.su.it.signuptool
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
@@ -15,6 +16,24 @@ class ActivateAccountAndCardServiceSpec extends Specification {
   def cleanup() {
   }
 
-  void "test something"() {
+  void "canOrderCard: Stub"() {
+    expect: 'will always return true'
+    service.canOrderCard()
   }
+  @Unroll
+  void "validateForwardAddress (invalid): \'#email\' "() {
+    expect: 'will return false'
+    !service.validateForwardAddress(email)
+    where:
+    email << [null, '', ' ', 'trixy', 'trixy@', '@trixy',
+        '!"#@!"#!', '!trixy@trixy.com', 'trixy@trixy!.com', 'trixy@trixy', 'trixy@trixy_.com']
+  }
+  @Unroll
+  void "validateForwardAddress (valid): \'#email\' "() {
+    expect: 'will return true'
+    service.validateForwardAddress(email)
+    where:
+    email << ['a@b.se', 'a@b.info', 'a.b@c.nu', 'a_b@cd.ef']
+  }
+
 }
