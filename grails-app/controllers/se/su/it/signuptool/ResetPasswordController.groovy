212d1ec130d56d06e9ea33f75ce192791cd6c29d
Magnus Norden <magnus.norden@su.se>
2013-07-04 12:37:44 +0200

    DEVIDM-25: some basics for reset passwd

--- grails-app/controllers/se/su/it/signuptool/ResetPasswordController.groovy
+++ grails-app/controllers/se/su/it/signuptool/ResetPasswordController.groovy
@@ -1,6 +1,9 @@
 package se.su.it.signuptool
 
+import se.su.it.svc.SvcSuPersonVO
+
 class ResetPasswordController {
+  def sukatService
   def utilityService
 
   def index() {
@@ -11,7 +14,20 @@ class ResetPasswordController {
     // Kör enrollUser
     // ... -> visa passwd mm..
     String scope = utilityService.getScopeFromEppn(request.eppn)
+    if(!(scope && (scope == 'studera.nu' || scope == 'antagning.se'))) {
+      log.error("scope (${scope}) must be defined and set to 'studera.nu' or 'antagning.se'!")
+      flash.error = "scope undefined or not equal to 'studera.nu' or 'antagning.se'!"
+      return render(view:'index')
+    }
+
     String norEduPersonNIN = request.norEduPersonNIN
+    if(!norEduPersonNIN) {
+      log.error("norEduPersonNIN not defined")
+      flash.error = "norEduPersonNIN not defined!"
+      return render(view:'index')
+    }
+
+    SvcSuPersonVO user = sukatService.findUserBySocialSecurityNumber()
 
     return render(view:'index')
   }
